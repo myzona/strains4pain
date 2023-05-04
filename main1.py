6571ea70c892007e9631ffd1344f30f6cfff3d54
myzona <myzona@gmail.com>
2023-05-04 11:44:23 -0700

    eryer5t

--- main1.py
+++ main1.py
@@ -49,7 +49,8 @@ with col1:
                 how they respond to stimuli, ultimately reducing pain.This tool \
                 will help you improve your pain by recommending best Canabbis strains for pain. This tool \
                 is powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com) and made by \
-                [@myzona](https://github.com/myzona). \n\n View Source Code on [Github](https://github.com/myzona/strains4pain1)")
+                [@myzona](https://github.com/myzona). \n\n \
+                View Source Code on [Github](https://github.com/myzona/strains4pain1)")
 
 with col2:
     st.image(image='img_strain1.png', width=500, caption='https://twitter.com/DannyRichman/status/1598254671591723008')
